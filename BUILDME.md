c1b75de9bb6f5e1f5931b0a41cbd887b8710eb4f
Angelo Polotto <angelo.polotto@tcs.com>
2020-12-04 14:51:24 -0300

    some point fixed

--- BUILDME.md
+++ BUILDME.md
@@ -5,13 +5,13 @@
 - install Node.js and NPM in your system;
 - open repository folder in the terminal and type: `npm install`
 - start developer environment with command: `npm start`
-- now you can edit files from `templates` folder, and the `README.md` file. All output files are in `dist` folder.
+- now you can edit files from `templates` folder, and the `README.md` file. The output file is `index.html` in the root.
 
 ## How the build works
 
 - read files in `templates` folder;
 - read `README.md` markdown file;
 - render `README.md` in to HTML;
-- put the redered HTML in to `dist` folder;
-- start live-serve to serve and reload automatically files from `dist` folder;
+- put the rendered HTML in to `index.html` file in the root;
+- start live-serve to serve and reload the browser automatically when `index.html` file in the root is changed;
 - watch `templates` folder and `README.md` for changes and repeat the markdown rendering process;
--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const path = require('path');
 const mustache = require('mustache');
 const liveServer = require("live-server");
 
+const params = {
+    port: 8080, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "./", // Set root directory that's being served. Defaults to cwd.
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    mount: [['./img', './favicon.ico']], // Mount a directory to a route.
+};
+
 function convertMarkdown(file) {
     const converter = new showdown.Converter();
 
@@ -27,16 +35,9 @@ function renderTemplates() {
 
     const indexRendered = renderHtml('./templates/index.html', view)
 
-    fs.writeFileSync('./dist/index.html', indexRendered, 'utf-8');
+    fs.writeFileSync('./index.html', indexRendered, 'utf-8');
 }
 
-const params = {
-    port: 8080, // Set the server port. Defaults to 8080.
-    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-    root: "./dist", // Set root directory that's being served. Defaults to cwd.
-    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-    mount: [['./img', './favicon.ico']], // Mount a directory to a route.
-};
 liveServer.start(params);
 
 fs.watch('./templates', (curr, prev) => {
